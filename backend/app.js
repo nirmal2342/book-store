861f6331e34a5b331ad895c040a4ab167ead4660
Nirmal Sugandhi <nirmal.sugandhi@softude.com>
2026-02-10 19:05:32 +0530

    Update app configuration and routes

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,12 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./src/config/db");
+
+dotenv.config();
+
 const app = express();
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Backend running");
