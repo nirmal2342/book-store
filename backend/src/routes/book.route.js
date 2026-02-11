ed30b00c9cee5233252aaaa80fd4383ec79a8ccf
Nirmal Sugandhi <nirmal.sugandhi@softude.com>
2026-02-11 17:14:34 +0530

    added swagger comments for book route

--- backend/src/routes/book.route.js
+++ backend/src/routes/book.route.js
@@ -9,6 +9,25 @@ const {
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Books
+ *   description: Book Management APIs
+ */
+
+/**
+ * @swagger
+ * /api/books:
+ *   get:
+ *     summary: Get all books
+ *     tags: [Books]
+ *     responses:
+ *       200:
+ *         description: List of books
+ */
+
+
 router.get("/", getAllBooks);
 router.get("/:id", getBookById);
 router.post("/", createBook);
