53941094374351e5ed1c4d8a80707b58bd834934
Nirmal Sugandhi <nirmal.sugandhi@softude.com>
2026-02-11 18:21:34 +0530

    db config file redefined

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,12 +1,18 @@
 const mongoose = require("mongoose");
 
+let isConnected = false;
+
 const connectDB = async () => {
+  if (isConnected) {
+    return;
+  }
+
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    isConnected = conn.connections[0].readyState;
+    console.log("MongoDB Connected");
   } catch (error) {
     console.error("MongoDB connection failed", error.message);
-    process.exit(1);
   }
 };
 
