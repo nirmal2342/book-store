3863fdcdcb29311bd1686db8c49641c282baadf5
Nirmal Sugandhi <nirmal.sugandhi@softude.com>
2026-02-11 16:11:27 +0530

    added cart routes

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const authRoutes = require("./src/routes/auth.route");
 const bookRoutes = require("./src/routes/book.route");
 const userRoutes = require("./src/routes/user.route");
+const cartRoutes = require("./src/routes/cart.route");
 
 const app = express();
 
@@ -15,5 +16,6 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/cart", cartRoutes);
 
 module.exports = app;
