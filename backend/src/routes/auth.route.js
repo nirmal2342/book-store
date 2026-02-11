735477517bcd02f64d72c69bb5e222bccb3388fe
Nirmal Sugandhi <nirmal.sugandhi@softude.com>
2026-02-11 17:13:11 +0530

    added swagger comments for auth route

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -3,6 +3,42 @@ const { registerUser, loginUser } = require("../controllers/auth.controller");
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Auth
+ *   description: Authentication APIs
+ */
+
+/**
+ * @swagger
+ * /api/auth/register:
+ *   post:
+ *     summary: Register a new user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *               - email
+ *               - password
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: User registered successfully
+ */
+
+
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 
