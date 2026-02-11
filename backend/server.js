efd864d2f8c9c156dbbb6f7d4cae7244fc7c0aba
Nirmal Sugandhi <nirmal.sugandhi@softude.com>
2026-02-11 14:19:13 +0530

    redefined server.js

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,19 @@ const app = require("./app");
 
 dotenv.config();
 
-connectDB();
+const startServer = async () => {
+  try {
+    await connectDB();
 
-const PORT = process.env.PORT || 3000;
+    const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () =>
-  console.log(`Server started on port ${PORT}`)
-);
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+startServer();
