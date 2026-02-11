bbf4c44dc04f6e3ed3c0103781e9a6554b6c5eda
Nirmal Sugandhi <nirmal.sugandhi@softude.com>
2026-02-11 17:21:06 +0530

    added swagger comments for cart route

--- backend/src/routes/cart.route.js
+++ backend/src/routes/cart.route.js
@@ -11,6 +11,86 @@ const { protect } = require("../middlewares/auth.middleware");
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Cart
+ *   description: Shopping Cart APIs
+ */
+
+/**
+ * @swagger
+ * /api/cart:
+ *   get:
+ *     summary: Get user cart
+ *     tags: [Cart]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: User cart
+ */
+
+/**
+ * @swagger
+ * /api/cart:
+ *   post:
+ *     summary: Add item to cart
+ *     tags: [Cart]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - bookId
+ *               - quantity
+ *             properties:
+ *               bookId:
+ *                 type: string
+ *               quantity:
+ *                 type: number
+ *     responses:
+ *       200:
+ *         description: Item added to cart
+ */
+
+/**
+ * @swagger
+ * /api/cart:
+ *   put:
+ *     summary: Update cart item quantity
+ *     tags: [Cart]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Cart updated
+ */
+
+/**
+ * @swagger
+ * /api/cart/{bookId}:
+ *   delete:
+ *     summary: Remove item from cart
+ *     tags: [Cart]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: bookId
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Item removed
+ */
+
+
 router.post("/", protect, addToCart);
 router.get("/", protect, getCart);
 router.put("/", protect, updateCartItem);
