ad8eed54f7cccbcb27b66fef5ec51fb5319aefa4
Nirmal Sugandhi <nirmal.sugandhi@softude.com>
2026-02-11 17:19:16 +0530

    updates swagger comments for book route

--- backend/src/routes/book.route.js
+++ backend/src/routes/book.route.js
@@ -13,7 +13,7 @@ const router = express.Router();
  * @swagger
  * tags:
  *   name: Books
- *   description: Book Management APIs
+ *   description: Book Catalog APIs
  */
 
 /**
@@ -26,6 +26,60 @@ const router = express.Router();
  *       200:
  *         description: List of books
  */
+
+/**
+ * @swagger
+ * /api/books:
+ *   post:
+ *     summary: Create new book
+ *     tags: [Books]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - title
+ *               - author
+ *               - price
+ *             properties:
+ *               title:
+ *                 type: string
+ *               author:
+ *                 type: string
+ *               genre:
+ *                 type: string
+ *               price:
+ *                 type: number
+ *               rating:
+ *                 type: number
+ *               stock:
+ *                 type: number
+ *     responses:
+ *       201:
+ *         description: Book created
+ */
+
+/**
+ * @swagger
+ * /api/books/{id}:
+ *   get:
+ *     summary: Get book by ID
+ *     tags: [Books]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Book details
+ */
+
 
 
 router.get("/", getAllBooks);
