b9ae36de39f10daf1c29c1dd2279f68d448bc2ab
Nirmal Sugandhi <nirmal.sugandhi@softude.com>
2026-02-11 17:21:53 +0530

    added swagger comments for order route

--- backend/src/routes/order.route.js
+++ backend/src/routes/order.route.js
@@ -10,6 +10,72 @@ const { protect } = require("../middlewares/auth.middleware");
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Orders
+ *   description: Order Management APIs
+ */
+
+/**
+ * @swagger
+ * /api/orders:
+ *   post:
+ *     summary: Place an order
+ *     tags: [Orders]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       201:
+ *         description: Order placed
+ */
+
+/**
+ * @swagger
+ * /api/orders:
+ *   get:
+ *     summary: Get order history
+ *     tags: [Orders]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: List of user orders
+ */
+
+/**
+ * @swagger
+ * /api/orders/{id}:
+ *   get:
+ *     summary: Get order by ID
+ *     tags: [Orders]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Order details
+ */
+
+/**
+ * @swagger
+ * /api/orders/{id}/cancel:
+ *   put:
+ *     summary: Cancel order
+ *     tags: [Orders]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Order cancelled
+ */
+
+
 router.post("/", protect, placeOrder);
 router.get("/", protect, getMyOrders);
 router.get("/:id", protect, getOrderById);
