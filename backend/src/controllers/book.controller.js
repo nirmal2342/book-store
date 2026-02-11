c60f2317b66ea1d27a331e2b80b26b97accb133b
Nirmal Sugandhi <nirmal.sugandhi@softude.com>
2026-02-11 12:12:12 +0530

    post route added for books

--- backend/src/controllers/book.controller.js
+++ backend/src/controllers/book.controller.js
@@ -9,3 +9,12 @@ exports.getBookById = async (req, res) => {
   const book = await Book.findById(req.params.id);
   res.json(book);
 };
+
+exports.createBook = async (req, res) => {
+  try {
+    const book = await Book.create(req.body);
+    res.status(201).json(book);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- backend/src/routes/book.route.js
+++ backend/src/routes/book.route.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { getAllBooks, getBookById } = require("../controllers/book.controller");
+const { getAllBooks, getBookById, createBook } = require("../controllers/book.controller");
 
 const router = express.Router();
 
 router.get("/", getAllBooks);
 router.get("/:id", getBookById);
+router.post("/", createBook);
 
 module.exports = router;
