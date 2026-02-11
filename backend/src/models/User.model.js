db1aecc0d9b3eb7b4ad6ac5d284b33196d0d7a2f
Nirmal Sugandhi <nirmal.sugandhi@softude.com>
2026-02-11 14:35:06 +0530

    User model redefinded with password saving and comparing features

--- backend/src/models/User.model.js
+++ backend/src/models/User.model.js
@@ -1,17 +1,16 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: true,
-      trim: true
+      required: true
     },
     email: {
       type: String,
       required: true,
-      unique: true,
-      lowercase: true
+      unique: true
     },
     password: {
       type: String,
@@ -25,5 +24,19 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Compare password
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 module.exports = mongoose.model("User", userSchema);
