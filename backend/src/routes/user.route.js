953df71057554767f2b9e01b6b8c837c509d5ce7
Nirmal Sugandhi <nirmal.sugandhi@softude.com>
2026-02-11 17:17:28 +0530

    added swagger comments for user route

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -8,6 +8,53 @@ const { protect } = require("../middlewares/auth.middleware");
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: User Profile APIs
+ */
+
+/**
+ * @swagger
+ * /api/users/profile:
+ *   get:
+ *     summary: Get logged-in user profile
+ *     tags: [Users]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: User profile
+ */
+
+/**
+ * @swagger
+ * /api/users/profile:
+ *   put:
+ *     summary: Update user profile
+ *     tags: [Users]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: false
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Profile updated
+ */
+
+
 router.get("/profile", protect, getUserProfile);
 router.put("/profile", protect, updateUserProfile);
 
