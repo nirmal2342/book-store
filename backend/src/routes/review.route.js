95a0757ef2accd1537c35c9a3b6e2f0feb0118dd
Nirmal Sugandhi <nirmal.sugandhi@softude.com>
2026-02-11 17:22:35 +0530

    added swagger comments for review route

--- backend/src/routes/review.route.js
+++ backend/src/routes/review.route.js
@@ -4,6 +4,64 @@ const { protect } = require("../middlewares/auth.middleware");
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Reviews
+ *   description: Book Review APIs
+ */
+
+/**
+ * @swagger
+ * /api/reviews/{bookId}:
+ *   post:
+ *     summary: Add review to a book
+ *     tags: [Reviews]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: bookId
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - rating
+ *               - comment
+ *             properties:
+ *               rating:
+ *                 type: number
+ *               comment:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Review added
+ */
+
+/**
+ * @swagger
+ * /api/reviews/{bookId}:
+ *   get:
+ *     summary: Get reviews for a book
+ *     tags: [Reviews]
+ *     parameters:
+ *       - in: path
+ *         name: bookId
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: List of reviews
+ */
+
+
 router.post("/:bookId", protect, addReview);
 router.get("/:bookId", getBookReviews);
 
